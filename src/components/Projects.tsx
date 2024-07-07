6161797b72b0e277e8d9228284a5da7ffd7628d5
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2024-07-07 22:10:17 +0900

    Feat: Projects 상세보기 기능 추가 및 배포링크 추가

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,37 +1,74 @@
 "use client";
 
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import { CardBody, CardContainer, CardItem } from "./ui/Card3d";
-
+import { motion } from "framer-motion";
 import triplusImg from "../../public/project-trplus.png";
 import secretLionImg from "../../public/project-secretLion.png";
+import profileImg from "../../public/portfoilo-profile.jpeg";
+
+import { FaExternalLinkAlt } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
 
 export default function Projects() {
-  const projects = [
+  const initialProjectsData = [
+    {
+      title: "Portfolio",
+      img: profileImg,
+      period: "(2024.07 ~ 2024.07)",
+      intro:
+        "개인 포트폴리오 웹 사이트입니다.\nframer-motion 라이브러리를 활용하여 인터랙티브하고 반응형 애니메이션을 구현",
+      member: "FE 1명",
+      contribute: "100",
+      skills: "Next.js, Tailwind CSS",
+      github: "https://github.com/munseunggyu/Secret-Lion",
+      detail: false
+    },
+    {
+      title: "triPlus",
+      img: triplusImg,
+      period: "(2022.12.09 ~ 2023.01.05)",
+      intro:
+        "triPlus는 다양한 사용자들의 폭넓은 여행 후기 및 자신의 여행 상품을 홍보할 수 있는 SNS서비스입니다.",
+      contents:
+        "1. 사용자 경험을 개선하고, 콘텐츠 노출을 극대화하귀 위해 무한 스크롤 구현\n2. 서버에 부하를 줄이기위해 검색 기능에 디바운싱 적용\n3. Custom Hooks를 이용하여 코드의 재사용성 증가\n4. 이미지 스프라이트 기법을 활용하여 자주 변경되지 않는 25개의 이미지를 하나의 이미지로 통합함으로써, 해당 이미지 로딩 시간 약 10배 가량 단축",
+      member: "FE 4명",
+      contribute: "39",
+      skills: "React, Styled-Components",
+      github: "https://github.com/munseunggyu/triplus",
+      detail: false
+    },
     {
       title: "Secret-Lion",
       img: secretLionImg,
       period: "(2022.11.03 ~ 2022.12.08)",
       intro:
-        "'비밀멋사'는 온라인으로 진행하는 부트캠프 훈련생간의 더욱 활발한 소통을 위한 익명 커뮤니티 서비스입니다.\n순수 자바스크립트로 SPA 구현",
-      member: "프론트엔드 4명",
+        "순수 자바스크립트로 SPA 구현한 '비밀멋사'는 온라인으로 진행하는 부트캠프 훈련생간의 더욱 활발한 소통을 위한 익명 커뮤니티 서비스입니다.",
+      contents:
+        "1. 싱글 페이지 애플리케이션(SPA) 기능을 위해 window.pushState와 window.onpopstate를 이용한 커스텀 라우터를 개발\n2. 상태관리와 렌더링 로직을 위해 initialize로 최초 상태 값과 컴포넌트를 저장한 이후 상태 값이 변경 되었을 경우 replaceWith 메서드를 사용하여 DOM을 효율적으로 업데이트",
+      member: "FE 4명",
       contribute: "20",
       skills: "JavaScript, Firebase",
-      github: "https://github.com/munseunggyu/Secret-Lion"
-    },
-    {
-      title: "triplus",
-      img: triplusImg,
-      period: "(2022.12.09 ~ 2023.01.05)",
-      intro:
-        "triPlus는 다양한 사용자들의 폭넓은 여행 후기 및 자신의 여행 상품을 홍보할 수 있는 SNS서비스입니다.\n\nReact로 처음 진행한 팀 프로젝트로 무한 스크롤기능과 디바운싱을 이용하여 최적화를 진행하였고 Custom Hooks를 이용하여 코드의 재사용성을 높였습니다.",
-      member: "프론트엔드 4명",
-      contribute: "39",
-      skills: "React, Styled-Components",
-      github: "https://github.com/munseunggyu/triplus"
+      github: "https://github.com/munseunggyu/Secret-Lion",
+      live: "https://secret-lion.netlify.app/",
+      detail: false
     }
   ];
+  const [projects, setProjects] = useState(initialProjectsData);
+
+  const toggleContent = (title: string) => {
+    const newProjects = projects.map(project => {
+      if (project.title === title) {
+        return {
+          ...project,
+          detail: !project.detail
+        };
+      }
+      return project;
+    });
+    setProjects(newProjects);
+  };
   return (
     <section id='projects' className='flex flex-col items-center pt-20'>
       <h2 className='text-center text-3xl mb-10 font-bold'>Projects</h2>
@@ -49,7 +86,7 @@ export default function Projects() {
                     {project.title}
                     <br />
                     <span className='text-xs mt-2  text-[#abb2b8]'>
-                      {"(2022.12.09 ~ 2023.01.05)"}
+                      {project.period}
                     </span>
                   </CardItem>
 
@@ -70,11 +107,37 @@ export default function Projects() {
                   >
                     {project.intro}
                   </CardItem>
+                  <CardItem
+                    as='button'
+                    translateZ='30'
+                    className='text-sm max-w-sm mt-4 text-neutral-300 underline'
+                    onClick={() => toggleContent(project.title)}
+                  >
+                    {project.detail ? "숨기기" : "상세 보기"}
+                  </CardItem>
+                  <CardItem
+                    as='div'
+                    translateZ='30'
+                    className='text-sm max-w-sm mt-4 text-neutral-300 whitespace-pre-wrap'
+                  >
+                    <motion.p
+                      initial={{ height: 0, opacity: 0 }}
+                      animate={{
+                        height: project.detail ? "auto" : 0,
+                        opacity: project.detail ? 1 : 0
+                      }}
+                      transition={{ duration: 0.5 }}
+                      style={{ overflow: "hidden" }}
+                    >
+                      {project.contents}
+                    </motion.p>
+                  </CardItem>
+
                   <hr className='my-4' />
                   <CardItem
                     as='span'
                     translateZ='40'
-                    className='text-xs mt-2  text-[#abb2b8]  block w-full'
+                    className='text-xs mt-2  text-[#abb2b8] leading-5 block w-full'
                   >
                     팀 구성: {project.member}
                     <br />
@@ -82,9 +145,18 @@ export default function Projects() {
                     <br />
                     기술스택: {project.skills}
                     <br />
-                    <a href={project.github} target='_blank'>
-                      깃허브: 링크
-                    </a>
+                    <br />
+                    <div className='flex gap-2 justify-end'>
+                      {" "}
+                      {project.live && (
+                        <a href={project.live} target='_blank'>
+                          <FaExternalLinkAlt size={20} />
+                        </a>
+                      )}
+                      <a href={project.github} target='_blank'>
+                        <FaGithub size={20} />
+                      </a>
+                    </div>
                   </CardItem>
                 </CardBody>
               </CardContainer>
