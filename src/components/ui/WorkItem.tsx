666235b9d093761c2e838f91133067d113b988cf
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2024-07-08 18:07:42 +0900

    update: Work Experience - border 색상 변경

--- src/components/ui/WorkItem.tsx
+++ src/components/ui/WorkItem.tsx
@@ -24,7 +24,7 @@ export const WorkItem = ({
   return (
     <div
       className={cn(
-        "row-span-1 relative overflow-hidden rounded-3xl border border-white/[0.1] group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4",
+        "row-span-1 relative overflow-hidden rounded-3xl border border-white/[0.2] group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4",
         className
       )}
     >
